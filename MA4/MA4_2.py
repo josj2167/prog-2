6c09e4aa010092018ca00003b31fc58e0817df4f
josj2167 <johan.sjoborg.2167student.uu.se>
2023-10-11 09:35:55 +0200

    mod ma4_2

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,12 +1,49 @@
 #!/usr/bin/env python3
 
 from person import Person
+from numba import njit
+import time
+import matplotlib.pyplot as plt
+
+n_values = list(range(30, 46))
+py_times = []
+numba_times = []
+cpp_times = []
+
+for n in n_values:
+    start_time = time.perf_counter()
+    fib_py(n)
+    py_times.append(time.perf_counter() - start_time)
+    
+    start_time = time.perf_counter()
+    fib_numba(n)
+    numba_times.append(time.perf_counter() - start_time)
+
+@njit
+def fib_numba(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+    
+def fib_py(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_py(n - 1) + fib_py(n - 2)
 
 def main():
 	f = Person(5)
 	print(f.get())
 	f.set(9)
 	print(f.get())
-
+	
 if __name__ == '__main__':
 	main()
