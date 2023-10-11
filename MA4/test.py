a75d38e47824c91fa7089a4e3b0f0f71cdc5c2de
Johan Sjöborg <johan.sjoborg.2167@student.uu.se>
2023-10-11 11:21:59 +0200

    afaw

--- MA4/test.py
+++ MA4/test.py
@@ -4,13 +4,14 @@ from person import Person
 # Create a Person object with an age
 f = Person(5)
 
-# Use the get method to get the age
-age = f.get()
+# Use the available methods in the Person class
+age = f.get()  # Use the get method to get the age
 print("Age:", age)
 
-# Use the set method to change the age
-f.set(9)
+f.set(9)  # Use the set method to change the age
 
-# Use the fibonacci method to calculate the Fibonacci number
+# If the Fibonacci function is available in the Person class, call it
+# However, make sure to use the correct method name based on the class definition in person.py
+# For example, if the method is named 'fibonacci' in the Person class:
 fib_result = f.fibonacci()
 print("Fibonacci Result:", fib_result)
