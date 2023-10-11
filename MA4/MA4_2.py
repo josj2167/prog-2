a6cba50b28428b8f031190a0e74298b4e52e3448
Johan Sjöborg <johan.sjoborg.2167@student.uu.se>
2023-10-11 09:30:03 +0200

    added fib and fibnumba

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 from person import Person
+from numba import njit
+
+@njit
+def fib_numba(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n + 1):
+            a, b = b, a + b
+        return b
+    
+def fib_py(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib_py(n - 1) + fib_py(n - 2)
 
 def main():
 	f = Person(5)
