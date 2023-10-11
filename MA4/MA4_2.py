be3def3da8ee06e5ac1a3702d4a80bf2e7070b91
Johan Sjöborg <johan.sjoborg.2167@student.uu.se>
2023-10-11 15:51:22 +0200

    awd

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -4,8 +4,6 @@ from person import Person
 import time
 from numba import njit
 
-n = list(range(30,46))
-
 def fib_py(n):
     if n <= 0:
         return 0
@@ -27,23 +25,25 @@ def fib_numba(n):
         return b
 
 if __name__ == '__main__':
-    start_time = time.perf_counter()
-    result = fib_py(n)
-    end_time = time.perf_counter()
-    print(f"Result (fib_py): {result}")
-    print(f"Execution Time (fib_py): {end_time - start_time} seconds")
+	for n in range(30,46):
+		start_time = time.perf_counter()
+		result = fib_py(n)
+		end_time = time.perf_counter()
+		print(f"Result (fib_py): {result}")
+		print(f"Execution Time (fib_py): {end_time - start_time} seconds")
 
-    start_time = time.perf_counter()
-    result = fib_numba(n)
-    end_time = time.perf_counter()
-    print(f"Result (fib_numba): {result}")
-    print(f"Execution Time (fib_numba): {end_time - start_time} seconds")
+		start_time = time.perf_counter()
+		result = fib_numba(n)
+		end_time = time.perf_counter()
+		print(f"Result (fib_numba): {result}")
+		print(f"Execution Time (fib_numba): {end_time - start_time} seconds")
     
-    person = Person(n)
-    start_time = time.perf_counter()
-    result = Person.fib(person, n)
-    end_time = time.perf_counter()
-    print(f"Result (fib_rec): {result}")
-    print(f"Execution Time (fib_rec): {end_time - start_time} seconds")
+		f = Person(n)
+		start_time = time.perf_counter()
+		result = f.fib(n)
+		end_time = time.perf_counter()
+		print(f"Result (fib_rec): {result}")
+		print(f"Execution Time (fib_rec): {end_time - start_time} seconds")
     
 
+#Person.fib(f, n)
